76bad348c0d7784a07d7a98a0f1829c2272114e7
Nashmei <168870570+Nashmei@users.noreply.github.com>
2026-09-20 10:35:00 +0300

    Add shared instance method to EOOverlayView

--- Sources/EOOverlayView.h
+++ Sources/EOOverlayView.h
@@ -1,8 +1,12 @@
 #import <UIKit/UIKit.h>
 
 @interface EOOverlayView : UIView
+
 + (instancetype)shared;
+
 - (void)installWhenReady;
+
 - (void)setStatus:(NSString *)status;
 - (void)setDetail:(NSString *)detail;
+
 @end
