68c6e68914e059fb140202bb6d3fd4738639e140
Nashmei <168870570+Nashmei@users.noreply.github.com>
2026-09-20 10:33:41 +0300

    Update Entry.mm

--- Sources/Entry.mm
+++ Sources/Entry.mm
@@ -1,9 +1,13 @@
 #import <Foundation/Foundation.h>
 #import "EOProbe.h"
 
-__attribute__((constructor)) static void EOSignalEntry(void) {
+__attribute__((constructor))
+static void EOSignalEntry(void) {
     @autoreleasepool {
         NSLog(@"[EOSignal] dylib loaded");
-        dispatch_async(dispatch_get_main_queue(), ^{ [EOProbe start]; });
+
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [EOProbe start];
+        });
     }
 }
